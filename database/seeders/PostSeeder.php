a74476c199b64e3bfc429b99643abb9e0c47e207
Christian <generalpride@gmail.com>
2024-01-13 22:05:15 +0800

    created posts factories, seeders and read json data as seed data

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,27 +2,21 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 
-class DatabaseSeeder extends Seeder
+class PostSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-
-        $this->call([
-            PostSeeder::class,
-            UserSeeder::class
-        ]);
-
-        // $json = File::get('database/json/posts.json');
-        // $posts = collect(json_decode($json));
+        // use this if you have many functions or seeders inside database seeder - for future proofing this is the best way, especially for large projects
+        $json = File::get('database/json/posts.json');
+        $posts = collect(json_decode($json));
 
 
         // $posts = collect([
@@ -44,15 +38,15 @@ class DatabaseSeeder extends Seeder
         //     ]
         // ]);
 
-        // $posts->each(function ($post) {
-        //     Post::create([
-        //         'title' => $post->title,
-        //         'slug' => $post->slug,
-        //         'excerpt' => $post->excerpt,
-        //         'description' => $post->description,
-        //         'is_published' => $post->is_published,
-        //         'min_to_read' => $post->min_to_read
-        //     ]);
-        // });
+        $posts->each(function ($post) {
+            Post::create([
+                'title' => $post->title,
+                'slug' => $post->slug,
+                'excerpt' => $post->excerpt,
+                'description' => $post->description,
+                'is_published' => $post->is_published,
+                'min_to_read' => $post->min_to_read
+            ]);
+        });
     }
 }
